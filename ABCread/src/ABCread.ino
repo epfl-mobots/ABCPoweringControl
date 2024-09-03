607fde822d937bddac1307e92b31a3bf69d2bd89
sirogo <cyril.sirogo@gmail.com>
2024-09-03 20:55:06 +0200

    clarified code

--- ABCread/src/ABCread.ino
+++ ABCread/src/ABCread.ino
@@ -9,7 +9,7 @@
 #define BAUD_RATE 115200
 
 #define M5_UNIT_AMETER_1_I2C_ADDR 0x48
-#define M5_UNIT_AMETER_2_I2C_ADDR 0x49 // Changed to 0x49 as per your comment
+#define M5_UNIT_AMETER_2_I2C_ADDR 0x49
 
 #define M5_UNIT_AMETER_EEPROM_I2C_ADDR 0x51
 #define M5_UNIT_AMETER_PRESSURE_COEFFICIENT 0.05
@@ -17,12 +17,12 @@
 #define SWITCH_1_PIN 25
 #define SWITCH_2_PIN 26
 
-#define SWITCH_ON HIGH
-#define SWITCH_OFF LOW
+#define SWITCH_CLOSED HIGH
+#define SWITCH_OPEN LOW
 
 const static bool verbose = false;
 const static bool display = true;
-static float maximal_current = 800; // maximal current in mA
+static float maximal_current = 2000; // maximal current in mA
 
 static ADS1115 Ameter_1;
 static float resolution_1 = 0.0;
@@ -54,17 +54,13 @@ void clearSerialBuffer() {
     Serial.flush();
 }
 
-String inputString = "";  
-bool stringComplete = false;
-
-
 void setup_switches(){
     pinMode(SWITCH_1_PIN, OUTPUT);
     pinMode(SWITCH_2_PIN, OUTPUT);
 
-    // Start with the pins HIGH or ON
-    digitalWrite(SWITCH_1_PIN, SWITCH_ON);
-    digitalWrite(SWITCH_2_PIN, SWITCH_ON);
+    // Start with the pins ON (CLOSED)
+    digitalWrite(SWITCH_1_PIN, SWITCH_CLOSED);
+    digitalWrite(SWITCH_2_PIN, SWITCH_CLOSED);
 }
 
 void setup_ammeters() {
@@ -119,20 +115,26 @@ float measure_current(ADS1115* Ameter, float resolution, float calibration_facto
 }
 
 void displayLatestCurrents(){
-    M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.setCursor(0, 0);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.setTextColor(YELLOW);
-    M5.Lcd.printf("Current 1: %.2f mA\n", get_current1());
-    M5.Lcd.printf("Current 2: %.2f mA\n", get_current2());
+    static float prev_c1,prev_c2 = -1.0;
+    if(prev_c1!=current_1 || prev_c2!=current_2){
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setCursor(10, 30);
+        M5.Lcd.setTextSize(2);
+        M5.Lcd.setTextColor(YELLOW);
+        M5.Lcd.printf("Current 1: %.2f mA\n", current_1);
+        M5.Lcd.setCursor(10, 100);
+        M5.Lcd.printf("Current 2: %.2f mA\n", current_2);
+        prev_c1=current_1;
+        prev_c2=current_2;
+    }
 }
 
-void limit_current(float current1, float current2){
-  if (abs(get_current1()) > maximal_current){
-    digitalWrite(SWITCH_1_PIN, SWITCH_OFF);
+void ensure_safe_currents(float current1, float current2){
+  if (abs(current_1) > maximal_current){
+    digitalWrite(SWITCH_1_PIN, SWITCH_OPEN);
   }
-  else if(abs(get_current2()) > maximal_current){
-    digitalWrite(SWITCH_2_PIN, SWITCH_OFF);
+  else if(abs(current_2) > maximal_current){
+    digitalWrite(SWITCH_2_PIN, SWITCH_OPEN);
   } 
 }
 
@@ -142,8 +144,8 @@ void command_handler(String command){
       // Serial.println(command);
       if (command == "Init"){ // clear the serial buffer  
         clearSerialBuffer();
-        digitalWrite(SWITCH_1_PIN, SWITCH_ON);  // Turn on SWITCH 1
-        digitalWrite(SWITCH_2_PIN, SWITCH_ON);  // Turn on SWITCH 2
+        digitalWrite(SWITCH_1_PIN, SWITCH_CLOSED);  // Turn on SWITCH 1
+        digitalWrite(SWITCH_2_PIN, SWITCH_CLOSED);  // Turn on SWITCH 2
         Serial.println("Buffer successfully reset, switches turned on");
       }
       else if (command == "Measure current 0")
@@ -175,39 +177,39 @@ void command_handler(String command){
         }
       else if (command == "Switch 1 on"){
       
-              digitalWrite(SWITCH_1_PIN, SWITCH_ON);  // Turn off SWITCH 1
-              Serial.println("SWITCH 1 turned on");
+              digitalWrite(SWITCH_1_PIN, SWITCH_CLOSED);  // Turn on SWITCH 1
+              if (verbose) Serial.println("SWITCH 1 closed (on)");
 
       }
       else if (command == "Switch 1 off"){
       
-              digitalWrite(SWITCH_1_PIN, SWITCH_OFF);  // Turn off SWITCH 1
-              Serial.println("SWITCH 1 turned off");
+              digitalWrite(SWITCH_1_PIN, SWITCH_OPEN);  // Turn off SWITCH 1
+              if (verbose) Serial.println("SWITCH 1 opened (off)");
 
       }
       else if (command == "Switch 2 on"){
       
-              digitalWrite(SWITCH_2_PIN, SWITCH_ON);  // Turn on SWITCH 1
-              Serial.println("SWITCH 2 turned on");
+              digitalWrite(SWITCH_2_PIN, SWITCH_CLOSED);  // Turn on SWITCH 1
+              if (verbose) Serial.println("SWITCH 2 closed (on)");
 
       }
       else if (command == "Switch 2 off"){
       
-              digitalWrite(SWITCH_2_PIN, SWITCH_OFF);  // Turn of SWITCH 1
-              Serial.println("SWITCH 2 turned off");
+              digitalWrite(SWITCH_2_PIN, SWITCH_OPEN);  // Turn of SWITCH 1
+              if (verbose) Serial.println("SWITCH 2 opened (off)");
 
       }
       else if (command == "Get switch 1 state"){
-        if (digitalRead(SWITCH_1_PIN) == SWITCH_OFF)
-             Serial.println("SWITCH 1 turned off");
-        else if (digitalRead(SWITCH_1_PIN) == SWITCH_ON)
-             Serial.println("SWITCH 1 turned on");
+        if (digitalRead(SWITCH_1_PIN) == SWITCH_OPEN)
+            if (verbose) Serial.println("SWITCH 1 open (off)");
+        else if (digitalRead(SWITCH_1_PIN) == SWITCH_CLOSED)
+            if (verbose) Serial.println("SWITCH 1 closed (on)");
       }
       else if (command == "Get switch 2 state"){
-        if (digitalRead(SWITCH_2_PIN) == SWITCH_OFF)
-             Serial.println("SWITCH 2 turned off");
-        else if (digitalRead(SWITCH_2_PIN) == SWITCH_ON)
-             Serial.println("SWITCH 2 turned on");
+        if (digitalRead(SWITCH_2_PIN) == SWITCH_OPEN)
+            if (verbose) Serial.println("SWITCH 2 open (off)");
+        else if (digitalRead(SWITCH_2_PIN) == SWITCH_CLOSED)
+            if (verbose) Serial.println("SWITCH 2 closed (on)");
       }
       else if (command.startsWith("Max current ")){
         String maxCurrentStr = command.substring(12); // 12 is length of "Max current "
@@ -230,11 +232,13 @@ void setup() {
     setup_switches();
     setup_ammeters();
 
-   clearSerialBuffer();
+    clearSerialBuffer();
 }
 
 
 void loop() {
+    static String inputString = "";  
+    static bool stringComplete = false;
     // Handle serial input
     while (Serial.available()) {
         char inChar = (char)Serial.read();
@@ -258,11 +262,12 @@ void loop() {
     if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;  // Update the previous time
 
-    current_1 = measure_current(&Ameter_1, resolution_1, calibration_factor_1);
-    current_2 = measure_current(&Ameter_2, resolution_2, calibration_factor_2);
-    if (verbose) Serial.printf("Current 1: %.2f mA, Current 2: %.2f mA\n", get_current1(), get_current2());
-    if (display) displayLatestCurrents();
-    limit_current(get_current1(), get_current2());  
-    delay(10);
+        current_1 = measure_current(&Ameter_1, resolution_1, calibration_factor_1);
+        current_2 = measure_current(&Ameter_2, resolution_2, calibration_factor_2);
+        if (verbose) Serial.printf("Current 1: %.2f mA, Current 2: %.2f mA\n", get_current1(), get_current2());
+        if (display) displayLatestCurrents();
+        ensure_safe_currents(get_current1(), get_current2());  // Open switches if current is too high
+        delay(10);
     }
+    delay(10);
 }
