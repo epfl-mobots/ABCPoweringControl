85a61eb410d491844d365617dceda891c51dfac8
sirogo <cyril.sirogo@gmail.com>
2024-11-08 09:27:42 +0100

    First attempt (buggy)

--- ABCread/src/ABCread.ino
+++ ABCread/src/ABCread.ino
@@ -1,24 +1,32 @@
 // Version Aug 27, working on all 3 boards, created by Raphael Kohler
 
+#include <Arduino.h>
 #include "M5StickCPlus.h"
 #include "M5_ADS1115.h"
-#include <Arduino.h>
+#include "M5_4Relay.h"
 
-#define BAUD_RATE 115200
+#define BAUD_RATE 115200 // To communicate with the RPi (host)
 
-#define M5_UNIT_AMETER_1_I2C_ADDR 0x48
-#define M5_UNIT_AMETER_2_I2C_ADDR 0x49
+#define M5_UNIT_MULTIPLEXER_I2C_ADDR 0x70
+#define M5_UNIT_AMETER_I2C_ADDR 0x48
+
+//I2C Mux channels
+#define AMPMETER_1_CH   3
+#define AMPMETER_2_CH   5
+#define RELAY_CH        4
+
+//Relay channels. The -1 is to convert the channel number to the index in the relay array
+#define ROBOT1_RELAY_CH 4 -1
+#define ROBOT2_RELAY_CH 3 -1
+#define LEDS_RELAY_CH   1 -1 
 
 #define M5_UNIT_AMETER_EEPROM_I2C_ADDR 0x51
 #define M5_UNIT_AMETER_PRESSURE_COEFFICIENT 0.05
 
-#define SWITCH_1_PIN 25
-#define SWITCH_2_PIN 26
+#define SWITCH_CLOSED   HIGH
+#define SWITCH_OPEN     LOW
 
-#define SWITCH_CLOSED HIGH
-#define SWITCH_OPEN LOW
-
-const static bool verbose = false;
+const static bool verbose = true;
 const static bool display = true;
 static float maximal_current = 2000; // maximal current in mA
 
@@ -32,16 +40,58 @@ static float resolution_2 = 0.0;
 static float calibration_factor_2 = 0.0;
 static float current_2 = 0.0;
 
-const long interval = 500;  // Interval to compare current (in milliseconds)
+static M5_4Relay relay; // The 4-Relay unit
 
+const long interval = 750;  // Interval to check new current value (in milliseconds)
 
-// Getter functions
-static float get_current1() {
-    return current_1;
+// Function to select the Multiplexer channel
+// input parameter: i: the channel number to select, between 0 and 5. The channels are as defined by the #define statements above
+void tcaselect(uint8_t i) {
+    if (i != 0 && i != 2 && i != 5) {
+        if (verbose) Serial.println("Invalid channel number");
+        return;
+    }
+    
+    Wire.beginTransmission(M5_UNIT_MULTIPLEXER_I2C_ADDR); 
+    Wire.write(1 << i); // select channel i 
+    Wire.endTransmission();  
 }
 
-static float get_current2() {
-    return current_2;
+// Function to scan all I2C addresses and print the addresses of the devices found or an error message
+// helps for debug
+void scanI2CAddresses() {
+    byte error, address;
+    int nDevices;
+    
+    Serial.println("Scanning...");
+
+    nDevices = 0;
+    for (address = 1; address < 127; address++) {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+
+        if (error == 0) {
+            Serial.print("I2C device found at address 0x");
+            if (address < 16) {
+                Serial.print("0");
+            }
+            Serial.print(address, HEX);
+            Serial.println(" !");
+            nDevices++;
+        } else if (error == 4) {
+            Serial.print("Unknown error at address 0x");
+            if (address < 16) {
+                Serial.print("0");
+            }
+            Serial.println(address, HEX);
+        }
+    }
+
+    if (nDevices == 0) {
+        Serial.println("No I2C devices found\n");
+    } else {
+        Serial.println("Done\n");
+    }
 }
 
 void clearSerialBuffer() {
@@ -51,19 +101,30 @@ void clearSerialBuffer() {
     Serial.flush();
 }
 
-void setup_switches(){
-    pinMode(SWITCH_1_PIN, OUTPUT);
-    pinMode(SWITCH_2_PIN, OUTPUT);
-
-    // Start with the pins ON (CLOSED)
-    digitalWrite(SWITCH_1_PIN, SWITCH_CLOSED);
-    digitalWrite(SWITCH_2_PIN, SWITCH_CLOSED);
+/*
+This function initialises the switch by ensuring that both switched of the robot are closed (ON).
+*/
+void init_switches(){
+    tcaselect(5); 
+    relay.begin(Wire); // Initialize the relay
+    relay.SyncMode(true); // Sync the LEDs with the relays
+    relay.AllOff(); // Turn off all relays
+    // Close switches ROBOT1_RELAY_CH and ROBOT2_RELAY_CH only
+    relay.Write4Relay(ROBOT1_RELAY_CH, SWITCH_CLOSED);
+    relay.Write4Relay(ROBOT2_RELAY_CH, SWITCH_CLOSED);
 }
 
-void setup_ammeters() {
-    while (!Ameter_1.begin(&Wire, M5_UNIT_AMETER_1_I2C_ADDR, 32, 33, 400000UL)) {
-        Serial.println("Unit Ameter 1 Init Fail");
-        delay(1000);
+/*
+This function initialises the ammeters by setting the resolution and calibration factors.
+*/
+void init_ammeters() {
+    scanI2CAddresses();
+    tcaselect(AMPMETER_1_CH); // Select the Ammeter 1 I2C channel
+    scanI2CAddresses();
+    delay(1000);
+    while (!Ameter_1.begin(&Wire, M5_UNIT_AMETER_I2C_ADDR)) {
+        if (verbose) Serial.println("Unit Ameter 1 Init Fail");
+        delay(2000);
     }
     Ameter_1.setEEPROMAddr(M5_UNIT_AMETER_EEPROM_I2C_ADDR);
     Ameter_1.setMode(ADS1115_MODE_SINGLESHOT);
@@ -79,9 +140,10 @@ void setup_ammeters() {
         Serial.println(calibration_factor_1);
     }
 
-        while (!Ameter_2.begin(&Wire, M5_UNIT_AMETER_2_I2C_ADDR, 32, 33, 400000UL)) {
-        Serial.println("Unit Ameter 2 Init Fail");
-        delay(1000);
+    tcaselect(AMPMETER_2_CH); // Select the Ammeter 2 I2C channel
+    while (!Ameter_2.begin(&Wire, M5_UNIT_AMETER_I2C_ADDR)) {
+        if (verbose) Serial.println("Unit Ameter 2 Init Fail");
+        delay(2000);
     }
     Ameter_2.setEEPROMAddr(M5_UNIT_AMETER_EEPROM_I2C_ADDR);
     Ameter_2.setMode(ADS1115_MODE_SINGLESHOT);
@@ -99,7 +161,8 @@ void setup_ammeters() {
 }
 
 
-float measure_current(ADS1115* Ameter, float resolution, float calibration_factor) {
+float measure_current(uint8_t I2C_ch, ADS1115* Ameter, float resolution, float calibration_factor) {
+    tcaselect(I2C_ch);
     int16_t adc_raw = Ameter->getSingleConversion();
     float current = adc_raw * resolution * calibration_factor;
 
@@ -128,105 +191,112 @@ void displayLatestCurrents(){
 
 void ensure_safe_currents(){
   if (abs(current_1) > maximal_current){
-    digitalWrite(SWITCH_1_PIN, SWITCH_OPEN);
+    tcaselect(RELAY_CH);
+    // Tell the relay to open channel ROBOT1_RELAY_CH
+    relay.Write4Relay(ROBOT1_RELAY_CH, SWITCH_OPEN);
   }
   else if(abs(current_2) > maximal_current){
-    digitalWrite(SWITCH_2_PIN, SWITCH_OPEN);
+    tcaselect(RELAY_CH);
+    // Tell the relay to open channel ROBOT2_RELAY_CH
+    relay.Write4Relay(ROBOT2_RELAY_CH, SWITCH_OPEN);
   } 
 }
 
-void command_handler(String command){
-      command.trim();
-      // Serial.print("Command was: ");
-      // Serial.println(command);
-      if (command == "Init"){ // clear the serial buffer  
+void command_handler(String command){ //TODO: change from 2-Relay to 4-Relay
+    command.trim();
+    if (verbose){
+        Serial.print("Received command: ");
+        Serial.println(command);
+    }
+
+    if (command == "Init"){ // clear the serial buffer  
         clearSerialBuffer();
-        digitalWrite(SWITCH_1_PIN, SWITCH_CLOSED);  // Turn on SWITCH 1
-        digitalWrite(SWITCH_2_PIN, SWITCH_CLOSED);  // Turn on SWITCH 2
+        init_switches();
         Serial.println("Buffer successfully reset, switches turned on");
-      }
-      else if (command == "Measure current 0")
-      {
+    }
+    else if (command == "Measure current 0"){
         Serial.print("Measuring current -> ");
-        current_1 = measure_current(&Ameter_1, resolution_1, calibration_factor_1);
-        current_2 = measure_current(&Ameter_2, resolution_2, calibration_factor_2);
+        current_1 = measure_current(AMPMETER_1_CH, &Ameter_1, resolution_1, calibration_factor_1);
+        current_2 = measure_current(AMPMETER_2_CH, &Ameter_2, resolution_2, calibration_factor_2);
         Serial.print("I1 = ");
-        Serial.print(get_current1());
+        Serial.print(current_1);
         Serial.print("mA, ");
         Serial.print("I2 = ");
-        Serial.print(get_current2());
+        Serial.print(current_2);
         Serial.println("mA");
-        }
-      else if (command == "Measure current 1")
-      {
+    }
+    else if (command == "Measure current 1"){
         Serial.print("Measuring current 1 -> ");
-        current_1 = measure_current(&Ameter_1, resolution_1, calibration_factor_1);
+        current_1 = measure_current(AMPMETER_1_CH, &Ameter_1, resolution_1, calibration_factor_1);
         Serial.print("I1 = ");
-        Serial.print(get_current1());
-        Serial.println("mA");        }
-      else if (command == "Measure current 2")
-      {
+        Serial.print(current_1);
+        Serial.println("mA");
+    }
+    else if (command == "Measure current 2"){
         Serial.print("Measuring current 2 -> ");
-        current_2 = measure_current(&Ameter_2, resolution_2, calibration_factor_2);
+        current_2 = measure_current(AMPMETER_2_CH, &Ameter_2, resolution_2, calibration_factor_2);
         Serial.print("I2 = ");
-        Serial.print(get_current2());
+        Serial.print(current_2);
         Serial.println("mA");        
+    }
+    else if (command.startsWith("Switch ")){
+        String switchNumberStr = command.substring(7); // 7 is length of "Switch "
+        int switchNumber = switchNumberStr.toInt(); // Convert to int
+        if (switchNumber < 1 || switchNumber > 4){
+            Serial.println("Invalid switch number");
+            return;
         }
-      else if (command == "Switch 1 on"){
-      
-              digitalWrite(SWITCH_1_PIN, SWITCH_CLOSED);  // Turn on SWITCH 1
-              Serial.println("SWITCH 1 closed (on)");
-
-      }
-      else if (command == "Switch 1 off"){
-      
-              digitalWrite(SWITCH_1_PIN, SWITCH_OPEN);  // Turn off SWITCH 1
-              Serial.println("SWITCH 1 opened (off)");
-
-      }
-      else if (command == "Switch 2 on"){
-      
-              digitalWrite(SWITCH_2_PIN, SWITCH_CLOSED);  // Turn on SWITCH 1
-              Serial.println("SWITCH 2 closed (on)");
-
-      }
-      else if (command == "Switch 2 off"){
-      
-              digitalWrite(SWITCH_2_PIN, SWITCH_OPEN);  // Turn of SWITCH 1
-              Serial.println("SWITCH 2 opened (off)");
-
-      }
-      else if (command == "Get switch 1 state"){
-        if (digitalRead(SWITCH_1_PIN) == SWITCH_OPEN)
-            Serial.println("SWITCH 1 open (off)");
-        else if (digitalRead(SWITCH_1_PIN) == SWITCH_CLOSED)
-            Serial.println("SWITCH 1 closed (on)");
-      }
-      else if (command == "Get switch 2 state"){
-        if (digitalRead(SWITCH_2_PIN) == SWITCH_OPEN)
-            Serial.println("SWITCH 2 open (off)");
-        else if (digitalRead(SWITCH_2_PIN) == SWITCH_CLOSED)
-            Serial.println("SWITCH 2 closed (on)");
-      }
-      else if (command.startsWith("Max current ")){
-        String maxCurrentStr = command.substring(12); // 12 is length of "Max current "
-        
+        if (command.endsWith("on")){
+            // Turn on the switch
+            tcaselect(RELAY_CH);
+            relay.Write4Relay(switchNumber - 1, SWITCH_CLOSED);
+            Serial.print("SWITCH ");
+            Serial.print(switchNumber);
+            Serial.println(" closed (on)");
+        }
+        else if (command.endsWith("off")){
+            // Turn off the switch
+            tcaselect(RELAY_CH);
+            relay.Write4Relay(switchNumber - 1, SWITCH_OPEN);
+            Serial.print("SWITCH ");
+            Serial.print(switchNumber);
+            Serial.println(" opened (off)");
+        }
+        else Serial.println("Command not recognized");
+    }
+    else if (command.startsWith("Get switch ") && command.endsWith(" state")){
+        String switchNumberStr = command.substring(11); // 11 is the start index of the switch number
+        int switchNumber = switchNumberStr.toInt(); // Convert to int
+        if (switchNumber < 1 || switchNumber > 4){
+            Serial.println("Invalid switch number");
+            return;
+        }
+        tcaselect(RELAY_CH);
+        int switchState = relay.ReadState() & (1 << (switchNumber - 1));
+        if (switchState == SWITCH_OPEN)
+            Serial.printf("SWITCH %d open (off)\n", switchNumber);
+        else if (switchState == SWITCH_CLOSED)
+            Serial.printf("SWITCH %d closed (on)\n", switchNumber);
+    }
+    else if (command.startsWith("Max current ")){
+        String maxCurrentStr = command.substring(12, command.length()); // 12 is length of "Max current "
         float maxCurrentValue = maxCurrentStr.toFloat(); // Convert to float        
         maximal_current = maxCurrentValue;
         Serial.print("Maximal current set to: ");
         Serial.println(maximal_current);
-      }
-      else {
-        Serial.println("Command not recognized");
-      }
+    }
+    else Serial.println("Command not recognized");
 }
 
 void setup() {
+    Wire.begin(32,33,400000UL); // SDA, SCL, frequency
+    delay(50);
+
     M5.begin();
     delay(2000);
     Serial.println("M5StickC started");
-    setup_switches();
-    setup_ammeters();
+    init_switches();
+    init_ammeters();
 
     clearSerialBuffer();
 }
@@ -253,18 +323,17 @@ void loop() {
         stringComplete = false;  
     }
 
-
     unsigned long currentMillis = millis();    
 
     if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;  // Update the previous time
 
-        current_1 = measure_current(&Ameter_1, resolution_1, calibration_factor_1);
-        current_2 = measure_current(&Ameter_2, resolution_2, calibration_factor_2);
+        current_1 = measure_current(AMPMETER_1_CH, &Ameter_1, resolution_1, calibration_factor_1);
+        current_2 = measure_current(AMPMETER_2_CH, &Ameter_2, resolution_2, calibration_factor_2);
         if (verbose) Serial.printf("Current 1: %.2f mA, Current 2: %.2f mA\n", current_1, current_2);
         if (display) displayLatestCurrents();
         ensure_safe_currents();  // Open switches if either current is too high
         delay(10);
     }
-    delay(10);
+    delay(20);
 }
